790c219a494cf1e6956444750f157580c190bed0
Najik7 <78047572+Najik7@users.noreply.github.com>
2021-02-01 14:39:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Д.з_6_2__alif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlayable playable = new Player();
+            IRecodable recodable = new Player();
+
+            playable.Play();
+            playable.Pause();
+            playable.Stop();
+
+            Console.WriteLine();
+            recodable.Record();
+            recodable.Pause();
+            recodable.Stop();
+        }
+ 
+    }
+
+
+
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+
+
+    }
+
+    public interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+
+    }
+
+
+    class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Play");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Pause");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stop");
+        }
+
+
+
+        public void Record()
+        {
+            Console.WriteLine("Record");
+        }
+
+
+    }
+
+}
